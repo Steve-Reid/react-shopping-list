3c056b98b3a55ad2a742f45b551db3b3687d30eb
SteveReid <steve@appby.design>
2022-08-08 16:01:11 +0100

    fix bag svg

--- src/components/Bag.tsx
+++ src/components/Bag.tsx
@@ -10,7 +10,14 @@ const Bag = () => {
         fill="none"
         viewBox="0 0 24 24"
         stroke="#fff"
-      />
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+          d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+        />
+      </svg>
     </div>
   );
 };
