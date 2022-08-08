1b9d9af70d16d0051201314213f48722f701dc32
SteveReid <steve@appby.design>
2022-08-08 16:09:39 +0100

    Add bag svg draw in animation on page load

--- src/components/Bag.tsx
+++ src/components/Bag.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
-// eslint-disable-next-line arrow-body-style
-const Bag = () => {
-  return (
-    <div className="bag-svg">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#fff"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="1"
-          d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-        />
-      </svg>
-    </div>
-  );
+const svgVariants = {
+  start: {
+    opacity: 0,
+    pathLength: 0
+  },
+  finished: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: 'easeInOut'
+    }
+  }
 };
+
+const Bag = () => (
+  <div className="bag-svg">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="#fff"
+    >
+      <motion.path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1"
+        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+        variants={svgVariants}
+        initial="start"
+        animate="finished"
+      />
+    </svg>
+  </div>
+);
 
 export default Bag;
